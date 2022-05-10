e0616e4cd96411701f9e927e08b569ebb355a815
mdk0121 <mdk0121@naver.com>
2022-05-10 13:31:47 +0900

    YG_GIT_F

--- 20220509.cpp
+++ 20220509.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int Sum(int* Sum, int SumNum)
+int ArraySum(int* Sum, int SumNum)
 {
 	int Result = 0;
 
@@ -14,6 +14,24 @@ int Sum(int* Sum, int SumNum)
 	return Result;
 }
 
+void Sort(int* Sort, int SortNum)
+{
+	int Temp = 0;
+
+	for (int i = 0; i < SortNum - 1; i++)
+	{
+		for (int j = 0; j < SortNum - 1; j++)
+		{
+			if (Sort[j] > Sort[j + 1])
+			{
+				Temp = Sort[j];
+				Sort[j] = Sort[j + 1];
+				Sort[j + 1] = Temp;
+				Temp = 0;
+			}
+		}
+	}
+}
 
 int main()
 {
@@ -21,7 +39,7 @@ int main()
 
 	cout << "얼마만큼 입력을 받으시겠습니까?: ";
 	cin >> Size;
-	int* Arr = new int[Size];
+	int* Arr = new int[Size];   // 사이즈 확정
 
 	for (int i = 0; i < Size; i++)
 	{
@@ -29,7 +47,17 @@ int main()
 		cin >> Arr[i];
 	}
 
-	cout << "합계: " << Sum(Arr, Size) << endl;
+	Sort(Arr, Size);   // 정렬
+
+	for (int i = 0; i < Size; i++)
+	{
+		cout << Arr[i] << " ";
+	}
+
+	cout << endl;
+	cout << "합계: " << ArraySum(Arr, Size) << endl;   // 배열 자체를 전달(배열은 자체가 포인터)
+
+	delete[] Arr;   // delete 꼭 해줘야 함.
 
 	return 0;
 }
